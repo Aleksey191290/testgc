a594484da60efa70cb67df89a8b34278cc4f0357
Aleksey <aleksey@chaldushkin.ddns.net>
2018-05-12 18:54:40 +0300

    v.1.0_begin

--- config/db.php
+++ config/db.php
@@ -3,8 +3,8 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=chaldushkin',
-    'username' => 'root',
-    'password' => 'chaldushkin',
+    'username' => 'username',
+    'password' => 'password',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
